a66a7d650c144ace75d6cb425f1cfaac0fb8ca0a
Aditi201022 <79974541+Aditi201022@users.noreply.github.com>
2021-12-04 12:15:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-/*https://teachablemachine.withgoogle.com/models/i4IMEOH1W/*/
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/i4IMEOH1W/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    console.log("Got result");
+}
